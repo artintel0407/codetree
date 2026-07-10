2856424b37ceff5cb387b40316c793e2a0f2e608
artintel0407 <artintel0407@gmail.com>
2026-07-10 22:47:55 +0900

    [Passed] 두 선분 (2ms, 0MB)

---
+++ trail2/두 선분/two-lines.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    if (b < c || d < a) cout << "nonintersecting";
+    else cout << "intersecting";
+    // Please write your code here.
+    return 0;
+}
