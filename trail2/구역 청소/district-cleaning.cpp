8c1bfb895769ea9cfd41ea6d8cda5dda7794b37a
artintel0407 <artintel0407@gmail.com>
2026-07-12 00:32:52 +0900

    [Passed] 구역 청소 (2ms, 0MB)

--- trail2/구역 청소/district-cleaning.cpp
+++ trail2/구역 청소/district-cleaning.cpp
@@ -8,8 +8,8 @@ int main() {
     if (b < c || d < a) cout << b - a + d - c;
     else if (a <= c && d <= b) cout << b - a;
     else if (c <= a && b <= d) cout << d - c;
-    else if (a <= c && c <= b && c <= b && b<= d) cout << d - a;
-    else if (c <= a && a <= d && a << d && d << b) cout << b - c;
+    else if (a <= c && c <= b && c <= b && b <= d) cout << d - a;
+    else if (c <= a && a <= d && a <= d && d <= b) cout << b - c;
     // Please write your code here.
     return 0;
 }
