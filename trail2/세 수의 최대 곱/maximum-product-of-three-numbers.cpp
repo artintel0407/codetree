5895407aef3b59128e7c94555a28056700b68aad
artintel0407 <artintel0407@gmail.com>
2026-07-19 02:58:52 +0900

    [Passed] 세 수의 최대 곱 (2ms, 0MB)

--- trail2/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
+++ trail2/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -1,65 +1,24 @@
-#include <iostream> // 33분 시작
+#include <iostream>
 #include <vector>
-#include <climits>
-#include <algorithm> // 43분 이해 완료
+#include <algorithm>
 using namespace std;
 
 int main() {
     int n;
     cin >> n;
-    vector<int> v(n);
 
+    vector<long long> a(n);
 
-    for (int& x : v) {
+    for (long long& x : a)
         cin >> x;
-    }
 
-    sort(v.begin(), v.end());
-    
-    int max_minus = INT_MIN;
-    int max_minus_idx = -1;
-    int min_plus = INT_MAX;
-    int min_plus_idx = -1;
-    int zero_idx = -1;
+    sort(a.begin(), a.end());
 
-    for (int i = 0; i < n; i++) {
-        if (v[i] < 0) {
-            if (max_minus < v[i]) {
-                max_minus = v[i];
-                max_minus_idx = i;
-            }
-        }
-        else if (v[i] == 0) {
-            zero_idx = i;
-        }
-        else {
-            if (min_plus > v[i]) {
-                min_plus = v[i];
-                min_plus_idx = i;
-            }
-        }
-    }
-
-    int ans = INT_MIN;
-    
-    if (zero_idx != -1) {
-        ans = max(ans, 0);
-    }
-
-    ans = max(ans, v[n - 1] * v[n - 2] * v[n - 3]);
-
-    ans = max(ans, v[0] * v[1] * v[n - 1]);
-
-    if (min_plus_idx != -1 && min_plus_idx + 1 < n && max_minus_idx != -1) {
-        ans = max(ans, v[min_plus_idx] * v[min_plus_idx + 1] * v[max_minus_idx]);
-    }
-
-    if (max_minus_idx != -1 && max_minus_idx - 2 >= 0) {
-        ans = max(ans, v[max_minus_idx] * v[max_minus_idx - 1] * v[max_minus_idx - 2]);
-    }
+    long long ans = max(
+        a[n - 1] * a[n - 2] * a[n - 3],
+        a[0] * a[1] * a[n - 1]
+    );
 
     cout << ans;
-    
-    // Please write your code here.
     return 0;
 }
